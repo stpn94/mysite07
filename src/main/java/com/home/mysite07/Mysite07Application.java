1bcbf582600c9adb61a96d56610c67658798a1e1
stpn94 <stpn94@gmail.com>
2022-02-17 18:24:29 +0900

    #ME [SET] (main) first commit

---
+++ src/main/java/com/home/mysite07/Mysite07Application.java
@@ -0,0 +1,13 @@
+package com.home.mysite07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mysite07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mysite07Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/home/mysite07/Mysite07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.mysite07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mysite07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
